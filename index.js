9e49a368e2abab28f66aec07fa0afc8ba2c0a855
TheD1TZ <83147115+TheD1TZ@users.noreply.github.com>
2022-09-21 09:22:27 +0200

    added more functions

--- index.js
+++ index.js
@@ -47,103 +47,103 @@ function getAllTracks(artistId) {
     return output;
 }
 
+function getArtist(artistId) {
+    const client_id = process.env.CLIENT_ID;
+    const client_secret = process.env.CLIENT_SECRET;
+    const b = Buffer.from(`${client_id}:${client_secret}`);
+
+    const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': 'Basic ' + b.toString('base64')
+        },
+        form: {
+            grant_type: 'client_credentials'
+        },
+        json: true
+    };
+
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            const reqOpts = {
+                headers: {
+                    Authorization: 'Bearer ' + body.access_token,
+                },
+                method: 'GET',
+                url: `https://api.spotify.com/v1/artists/${artistId}`,
+                json: true
+            };
+
+            request(reqOpts, processAllTracksResponse);
+
+            function processAllTracksResponse(error, res, body) {
+                if (error) {
+                    return console.log(error);
+                } else if (body.error) {
+                    return console.log('SpotifyScraper error: ' + body.error.message);
+                } else {
+                    console.log(body);
+                    return body;
+                }
+            }
+        }
+    });
+}
+
+function getTopTracks() {
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var reqOpts = {
+                headers: {
+                    Authorization: 'Bearer ' + body.access_token,
+                },
+                method: 'GET',
+                url: `https://api.spotify.com/v1/artists/${artistId}/getTopTracks?country=${country}`,
+                json: true
+            };
+
+            request(reqOpts, processTracksResponse);
+
+            function processTracksResponse(error,res, body) {
+                if (error) {
+                    return console.log(error);
+                } else {
+                    console.log(body);
+                    // return body;
+                }
+            }
+
+        }
+    });
+}
+
+function getRelatedArtists() {
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var reqOpts = {
+                headers: {
+                    Authorization: 'Bearer ' + body.access_token,
+                },
+                method: 'GET',
+                url: `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
+                json: true
+            };
+
+            request(reqOpts, processAllTracksResponse);
+
+            function processAllTracksResponse(error, res, body) {
+                if (error) {
+                    return console.log(error);
+                } else {
+                    return body;
+                }
+            }
+
+        }
+    });
+}
 
 
 
-module.exports = getAllTracks;
 
-// function getArtist(artistId) {
-//     const client_id = process.env.CLIENT_ID;
-//     const client_secret = process.env.CLIENT_SECRET;
-//     const b = Buffer.from(`${client_id}:${client_secret}`);
-//
-//     const authOptions = {
-//         url: 'https://accounts.spotify.com/api/token',
-//         headers: {
-//             'Authorization': 'Basic ' + b.toString('base64')
-//         },
-//         form: {
-//             grant_type: 'client_credentials'
-//         },
-//         json: true
-//     };
-//
-//     request.post(authOptions, function (error, response, body) {
-//         if (!error && response.statusCode === 200) {
-//             const reqOpts = {
-//                 headers: {
-//                     Authorization: 'Bearer ' + body.access_token,
-//                 },
-//                 method: 'GET',
-//                 url: `https://api.spotify.com/v1/artists/${artistId}`,
-//                 json: true
-//             };
-//
-//             request(reqOpts, processAllTracksResponse);
-//
-//             function processAllTracksResponse(error, res, body) {
-//                 if (error) {
-//                     return console.log(error);
-//                 } else if (body.error) {
-//                     return console.log('SpotifyScraper error: ' + body.error.message);
-//                 } else {
-//                     console.log(body);
-//                     return body;
-//                 }
-//             }
-//         }
-//     });
-// }
-
-// function getTopTracks() {
-//     request.post(authOptions, function (error, response, body) {
-//         if (!error && response.statusCode === 200) {
-//             var reqOpts = {
-//                 headers: {
-//                     Authorization: 'Bearer ' + body.access_token,
-//                 },
-//                 method: 'GET',
-//                 url: `https://api.spotify.com/v1/artists/${artistId}/getTopTracks?country=${country}`,
-//                 json: true
-//             };
-//
-//             request(reqOpts, processTracksResponse);
-//
-//             function processTracksResponse(error,res, body) {
-//                 if (error) {
-//                     return console.log(error);
-//                 } else {
-//                     console.log(body);
-//                     // return body;
-//                 }
-//             }
-//
-//         }
-//     });
-// }
-
-// function getRelatedArtists() {
-//     request.post(authOptions, function (error, response, body) {
-//         if (!error && response.statusCode === 200) {
-//             var reqOpts = {
-//                 headers: {
-//                     Authorization: 'Bearer ' + body.access_token,
-//                 },
-//                 method: 'GET',
-//                 url: `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
-//                 json: true
-//             };
-//
-//             request(reqOpts, processAllTracksResponse);
-//
-//             function processAllTracksResponse(error, res, body) {
-//                 if (error) {
-//                     return console.log(error);
-//                 } else {
-//                     return body;
-//                 }
-//             }
-//
-//         }
-//     });
-//}
+module.exports = getAllTracks, getArtist, getRelatedArtists, getTopTracks
